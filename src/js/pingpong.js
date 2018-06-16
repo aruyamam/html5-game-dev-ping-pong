d2c71560da6c1a63ffccb386eb5ac30e85508248
aruyamam <aruyamam@gmail.com>
2018-06-16 14:14:38 +0900

    Placing Ping Pong game elements in the DOM

--- src/js/pingpong.js
+++ src/js/pingpong.js
@@ -1,3 +1,26 @@
 import $ from 'jquery';
 
-$('#game').text('Hello jQuery');
+const pingpong = {
+  // data definition
+  paddleLeft: {
+    x: 50,
+    y: 100,
+    width: 20,
+    height: 70,
+  },
+
+  paddleRight: {
+    x: 320,
+    y: 50,
+    width: 20,
+    height: 70,
+  },
+};
+
+// view rendering
+function renderPaddles() {
+  $('#paddle-left').css('top', pingpong.paddleLeft.y);
+  $('#paddle-right').css('top', pingpong.paddleRight.y);
+}
+
+renderPaddles();
